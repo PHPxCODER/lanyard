107c9e7c68a4d8cf321b67114ed097fd1665b67a
Subhadeep Pramanik <codevizag@gmail.com>
2026-03-22 13:15:00 +0530

    Migrate Discord API module from HTTPoison+Poison to Finch+Jason
    Replace all HTTPoison.post/put calls and Poison.encode!/decode! with
    shared private helpers backed by Finch and Jason. Fixes create_dm to
    return {:ok, id} or {:error, reason} tuples instead of crashing on
    missing id.

--- lib/bot/discord_api.ex
+++ lib/bot/discord_api.ex
@@ -1,60 +1,54 @@
 defmodule Lanyard.DiscordBot.DiscordApi do
   @api_host "https://discord.com/api/v10"
 
+  defp headers(extra \\ []) do
+    [
+      {"Authorization", "Bot " <> Application.get_env(:lanyard, :bot_token)},
+      {"Content-Type", "application/json"}
+    ] ++ extra
+  end
+
+  defp post(url, body, opts \\ []) do
+    Finch.build(:post, url, headers(opts), Jason.encode!(body))
+    |> Finch.request(Lanyard.Finch)
+  end
+
+  defp put(url, body) do
+    Finch.build(:put, url, headers(), Jason.encode!(body))
+    |> Finch.request(Lanyard.Finch)
+  end
+
   def send_message(channel_id, content) when is_binary(content) do
     Lanyard.Metrics.Collector.inc(:counter, :lanyard_discord_messages_sent)
 
-    sanitized_content =
-      content
-      |> String.replace("@", "@​\u200b")
+    sanitized_content = String.replace(content, "@", "@​\u200b")
 
-    HTTPoison.post(
-      "#{@api_host}/channels/#{channel_id}/messages",
-      Poison.encode!(%{content: sanitized_content}),
-      [
-        {"Authorization", "Bot " <> Application.get_env(:lanyard, :bot_token)},
-        {"Content-Type", "application/json"}
-      ]
-    )
+    post("#{@api_host}/channels/#{channel_id}/messages", %{content: sanitized_content})
   end
 
   def register_global_commands(app_id, commands) do
-    HTTPoison.put(
-      "#{@api_host}/applications/#{app_id}/commands",
-      Poison.encode!(commands),
-      [
-        {"Authorization", "Bot " <> Application.get_env(:lanyard, :bot_token)},
-        {"Content-Type", "application/json"}
-      ]
-    )
+    put("#{@api_host}/applications/#{app_id}/commands", commands)
   end
 
   def register_slash_commands(app_id, guild_id, commands) do
-    HTTPoison.put(
-      "#{@api_host}/applications/#{app_id}/guilds/#{guild_id}/commands",
-      Poison.encode!(commands),
-      [
-        {"Authorization", "Bot " <> Application.get_env(:lanyard, :bot_token)},
-        {"Content-Type", "application/json"}
-      ]
-    )
+    put("#{@api_host}/applications/#{app_id}/guilds/#{guild_id}/commands", commands)
   end
 
   def respond_to_interaction(interaction_id, interaction_token, content, ephemeral \\ false) do
     flags = if ephemeral, do: 64, else: 0
 
-    HTTPoison.post(
+    post(
       "#{@api_host}/interactions/#{interaction_id}/#{interaction_token}/callback",
-      Poison.encode!(%{type: 4, data: %{content: content, flags: flags}}),
-      [{"Content-Type", "application/json"}]
+      %{type: 4, data: %{content: content, flags: flags}},
+      []
     )
   end
 
   def respond_with_modal(interaction_id, interaction_token, modal) do
-    HTTPoison.post(
+    post(
       "#{@api_host}/interactions/#{interaction_id}/#{interaction_token}/callback",
-      Poison.encode!(%{type: 9, data: modal}),
-      [{"Content-Type", "application/json"}]
+      %{type: 9, data: modal},
+      []
     )
   end
 
@@ -62,30 +56,23 @@ defmodule Lanyard.DiscordBot.DiscordApi do
     # 32768 = IS_COMPONENTS_V2 flag (1 <<< 15); combined with ephemeral (64) = 32832
     flags = if ephemeral, do: 32832, else: 32768
 
-    HTTPoison.post(
+    post(
       "#{@api_host}/interactions/#{interaction_id}/#{interaction_token}/callback",
-      Poison.encode!(%{type: 4, data: %{flags: flags, components: components}}),
-      [{"Content-Type", "application/json"}]
+      %{type: 4, data: %{flags: flags, components: components}},
+      []
     )
   end
 
   def create_dm(recipient) do
-    {:ok, response} =
-      HTTPoison.post(
-        "#{@api_host}/users/@me/channels",
-        Poison.encode!(%{recipient_id: recipient}),
-        [
-          {"Authorization", "Bot " <> Application.get_env(:lanyard, :bot_token)},
-          {"Content-Type", "application/json"}
-        ]
-      )
+    case post("#{@api_host}/users/@me/channels", %{recipient_id: recipient}) do
+      {:ok, %Finch.Response{body: body}} ->
+        case Jason.decode!(body) do
+          %{"id" => id} -> {:ok, id}
+          _ -> {:error, :missing_id}
+        end
 
-    case Poison.decode!(response.body) do
-      %{"id" => id} ->
-        {:ok, id}
-
-      _ ->
-        {:error, :missing_id}
+      {:error, reason} ->
+        {:error, reason}
     end
   end
 end
